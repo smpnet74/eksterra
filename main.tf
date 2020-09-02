5972c463914be4d2cf7f54d1673c08ac84bbc2e9
Scott Peterson <sm_peterson@yahoo.com>
2020-09-02 15:03:45 -0500

    add command to download in destroy resource

--- main.tf
+++ main.tf
@@ -18,8 +18,10 @@ resource "null_resource" "destroyeks" {
   provisioner "local-exec" {
     when    = destroy
     command = <<EOH
+     wget https://github.com/weaveworks/eksctl/releases/download/0.26.0/eksctl_Linux_amd64.tar.gz
+     tar -zxf ./eksctl_Linux_amd64.tar.gz
+     rm ./eksctl_Linux_amd64.tar.gz
      ./eksctl delete cluster -f eksctl.yaml
-     rm ./eksctl
  EOH
   }
 }
